4b7bb1855fb7adba6131adcaa661ec3bdfe24785
Juan Robledo <145608430+Juan-P-Robledo@users.noreply.github.com>
2024-04-26 16:49:19 -0500

    Added View users with Edit Users page

---
+++ DiscGolfWeb/Pages/Account/EditUsers.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DiscGolfWeb.Pages.Account
+{
+    public class EditUsersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
