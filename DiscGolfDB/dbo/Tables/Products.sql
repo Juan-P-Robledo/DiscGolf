929df8724d348cfe1239873c29d761d49998b872
Juan Robledo <145608430+Juan-P-Robledo@users.noreply.github.com>
2024-04-26 12:10:07 -0500

    Updated URL Length Acceptance

--- DiscGolfDB/dbo/Tables/Products.sql
+++ DiscGolfDB/dbo/Tables/Products.sql
@@ -5,13 +5,15 @@
     [Name]        VARCHAR (50)    NOT NULL,
     [Brand]       VARCHAR (50)    NOT NULL,
     [Price]       DECIMAL (18, 2) NOT NULL,
-    [Image]       VARCHAR (100)   NULL,
+    [Image]       VARCHAR (300)   NULL,
     [Description] VARCHAR (200)   NOT NULL,
     [CategoryID]  INT             NOT NULL,
     CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED ([ProductID] ASC),
     CONSTRAINT [FK_Products_Category] FOREIGN KEY ([CategoryID]) REFERENCES [dbo].[Category] ([CategoryID]),
     CONSTRAINT [FK_Products_Videos] FOREIGN KEY ([VideoID]) REFERENCES [dbo].[Videos] ([VideoID])
 );
+
+
 
 
 
