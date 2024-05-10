fe6c40109d7c1c604fd3ac3dc234770381902eb2
Juan Robledo <145608430+Juan-P-Robledo@users.noreply.github.com>
2024-05-10 01:31:54 -0500

    Fixed some Register cs file to fix View Users error.

--- DiscGolfWeb/Pages/Account/AdminEditPassword.cshtml.cs
+++ DiscGolfWeb/Pages/Account/AdminEditPassword.cshtml.cs
@@ -41,7 +41,7 @@ namespace DiscGolfWeb.Pages.Account
 
                     conn.Open();
                     cmd.ExecuteNonQuery();
-                    return RedirectToPage("Profile");
+                    return RedirectToPage("ViewUsers");
                 }
             }
             else
@@ -51,93 +51,7 @@ namespace DiscGolfWeb.Pages.Account
                              
         }
 
-        private IActionResult ProcessChangePassword()
-        {
-
-            using (SqlConnection conn = new SqlConnection(SecurityHelper.GetDBConnectionString()))
-            {
-
-                string cmdText = "SELECT Password FROM Users WHERE Email=@email";
-                SqlCommand cmd = new SqlCommand(cmdText, conn);
-                cmd.Parameters.AddWithValue("@email", Person.Email);
-                conn.Open();
-                SqlDataReader reader = cmd.ExecuteReader();
-                if (reader.HasRows)
-                {
-                    reader.Read();
-                    if (!reader.IsDBNull(0))
-                    {
-                        string passwordHash = reader.GetString(0);
-                        if (SecurityHelper.VerifyPassword(Person.Password, passwordHash))
-                        {
-                            return RedirectToPage("/Index");
-                        }
-                        else
-                        {
-                            ModelState.AddModelError("LoginError", "Invalid credentials. Try Again");
-                            return Page();
-                        }
-                    }
-                    else
-                    {
-                        ModelState.AddModelError("LoginError", "Invalid credentials. Try Again");
-                        return Page();
-                    }
-
-                }
-                else
-                {
-                    ModelState.AddModelError("LoginError", "Invalid credentials. Try Again");
-                    return Page();
-                }
-            }
-
-        }
-        private bool ValidateCredentials(string ConfirmPassword, int id)
-        {
-            using (SqlConnection conn = new SqlConnection(SecurityHelper.GetDBConnectionString()))
-            {
-
-
-                //  string cmdText = "SELECT Password FROM Users WHERE Email=@email";
-                string cmdText = "SELECT Password, UserID, FirstName, isAdmin FROM Users WHERE UserID=@userId";
-
-                SqlCommand cmd = new SqlCommand(cmdText, conn);
-
-                
-                cmd.Parameters.AddWithValue("@userId", id);
-                conn.Open();
-                SqlDataReader reader = cmd.ExecuteReader();
-
-                if (reader.HasRows)
-                {
-                    reader.Read();
-                    if (reader.IsDBNull(0))
-                    {
-                        return false;
-                    }
-                    else
-                    {
-                        string passwordHash = reader.GetString(0);
-                        if (SecurityHelper.VerifyPassword(ConfirmPassword, passwordHash))
-                        {
-                            return true;
-                        }
-                        else
-                        {
-                            return false;
-                        }
-                    }
-
-
-                }
-                else
-                {
-                    return false;
-                }
-
-            }
-        }
+        
 
 
         private void PopulateUser(int id)
